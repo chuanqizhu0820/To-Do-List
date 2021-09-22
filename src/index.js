16a674df421e2f815ed703daf262bfafbf2b587a
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-22 05:35:15 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import './style.css';
 
-let tasks = [{
-    description: "something to do",
-    completed: false,
-    index: 0
-  },
-  {
-    description: "something to do",
-    completed: false,
-    index: 1
-  },
-  {
-    description: "something to do",
-    completed: false,
-    index: 2
-  }
-]
+const tasks = [{
+  description: 'something to do',
+  completed: false,
+  index: 0,
+},
+{
+  description: 'something to do',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'something to do',
+  completed: false,
+  index: 2,
+},
+];
 
 const htmldiv = document.querySelector('.itemhtml');
 
-
 let itemHtml = '';
 tasks.forEach((item) => {
-  itemHtml +=
-    `<div class="item-container">
+  itemHtml
+    += `<div class="item-container">
      <div class="form-item">
      <input type="checkbox" id="${item.index}" name="todo" value="something">
      <label for="${item.index}">${item.description}</label>
@@ -31,7 +30,7 @@ tasks.forEach((item) => {
      <div class="dot-menu">
      <img src="https://img.icons8.com/fluency-systems-regular/48/000000/menu-2.png" />
      </div>
-     </div>`
+     </div>`;
 });
 
 htmldiv.innerHTML = itemHtml;
