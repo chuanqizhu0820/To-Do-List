9d6f6cf0cc34993aaf7b5a618273b0b460af96dd
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-22 11:25:13 +0100

    Update the completed status in the local storage

--- src/tasks.js
+++ src/tasks.js
@@ -1,14 +1,3 @@
-function changeStatus(arr){
-  const checkBoxes = document.querySelectorAll(".form-item input");
-  checkBoxes.forEach((item, i) => {
-    item.addEventListener("click", ()=>{
-      if (arr[i].completed){
-        arr[i].completed = false;
-      } else {
-        arr[i].completed = true;
-      }
-    });
-  });
-}
 
-export {changeStatus};
+
+// export {changeStatus};
