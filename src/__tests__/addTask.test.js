cd5a48079d2fdc394ebf55af7f7eb675d344a484
ntzwilly <ntzwilly@gmail.com>
2021-09-29 15:39:01 +0200

    add test for addTask function

---
+++ src/__tests__/addTask.test.js
@@ -0,0 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { add } from 'lodash';
+import addTask from '../__mocks__/addTask.js';
+import localStorageMock from '../__mocks__/localStorage.js';
+
+describe('Add to-do', () => {
+  document.body.innerHTML = '<input id="input-text" value="Something">';
+  const arr = [];
+  test('Should return an array with attached todo object', () => {
+    expect(addTask(arr)).toHaveLength(1);
+  });
+  test('Local storage should be updated after adding new item', () => {
+    expect(localStorageMock.getItem('data')).toHaveLength(1);
+  });
+  test('Should return an array with attached todo object', () => {
+    expect(addTask(arr)).toHaveLength(2);
+  });
+  test('The new task completed status should be false', () => {
+    expect(arr[0].completed).toBe(false);
+  });
+});
