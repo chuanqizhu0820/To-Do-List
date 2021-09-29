f7d3c7cc9e92ec18891bf6e419ee4309e982259c
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-29 16:14:39 +0100

    Add some note in addTask.test.js

--- src/__tests__/addTask.test.js
+++ src/__tests__/addTask.test.js
@@ -1,20 +1,20 @@
-/**
+/*
  * @jest-environment jsdom
  */
-
 import addTask from '../__mocks__/addTask.js';
 import localStorageMock from '../__mocks__/localStorage.js';
 
 describe('Add to-do', () => {
-  document.body.innerHTML = '<input id="input-text" value="Something">';
   const arr = [];
   test('Should return an array with attached todo object', () => {
+    document.body.innerHTML = '<input id="input-text" value="Something">';
     expect(addTask(arr)).toHaveLength(1);
   });
   test('Local storage should be updated after adding new item', () => {
     expect(localStorageMock.getItem('data')).toHaveLength(1);
   });
   test('Should return an array with attached todo object', () => {
+    document.body.innerHTML = '<input id="input-text" value="Something">';
     expect(addTask(arr)).toHaveLength(2);
   });
   test('The new task completed status should be false', () => {
