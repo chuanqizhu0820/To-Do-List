ef555c1fe2b6368b9d977df6cd790d9ea506d70c
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-09-30 13:45:12 +0100

    Add tests for the changeStatus mock function

---
+++ src/__tests__/changeStatus.test.js
@@ -0,0 +1,36 @@
+import changeStatus from "../__mocks__/changeStatus";
+import localStorageMock from "../__mocks__/localStorage";
+
+describe("test changeStatus function", () => {
+    const arr = [
+        {
+            description: 'something to do 1',
+            completed: true,
+            index: 1,
+        },
+        {
+            description: 'something to do 2',
+            completed: false,
+            index: 2,
+        },
+        {
+            description: 'something to do 3',
+            completed: true,
+            index: 3,
+        },
+    ];
+
+    test("should retrun arr with undated completed status in object", () => {
+        expect(changeStatus(arr, 0)[0].completed).toBe(false);
+    })
+    test("should update the localStorage with the array above ", () => {
+        expect(localStorageMock.getItem('data')[0].completed).toBe(false);
+    })
+    test("should retrun arr with undated completed status in object", () => {
+        expect(changeStatus(arr, 1)[1].completed).toBe(true);
+    })
+    test("should update the localStorage with the array above ", () => {
+        expect(localStorageMock.getItem('data')[1].completed).toBe(true);
+    })
+
+})
