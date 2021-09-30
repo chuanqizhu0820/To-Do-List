c9f308ad501e6dbfacc278f5d468a8ed8e68f9de
ntzwilly <ntzwilly@gmail.com>
2021-09-30 15:26:24 +0200

    Fix linter errors

--- src/__mocks__/changeStatus.js
+++ src/__mocks__/changeStatus.js
@@ -1,13 +1,13 @@
-import localStorageMock from "./localStorage";
+import localStorageMock from './localStorage';
 
 const changeStatus = (arr, index) => {
-    if (arr[index].completed) {
-        arr[index].completed = false;
-    } else {
-        arr[index].completed = true;
-    }
-    localStorageMock.setItem("data", arr);
-    return arr;
+  if (arr[index].completed) {
+    arr[index].completed = false;
+  } else {
+    arr[index].completed = true;
+  }
+  localStorageMock.setItem('data', arr);
+  return arr;
 };
 
 export default changeStatus;
